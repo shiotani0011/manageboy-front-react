cd4ef9922eb0112b6d11240f6822971d12abd4c8
shiotani0011 <76035510+kizuneko1@users.noreply.github.com>
2021-03-06 18:54:27 +0900

    router components

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
+import { Routes } from "./routes/Routes";
 
 // components
-import { Auth } from "./containers/Auth.jsx";
-import { Index } from "./containers/Index.jsx";
-import { Show } from "./containers/Show.jsx";
-import { Create } from "./containers/Create.jsx";
-import { Login } from "./containers/Login";
 
 function App() {
-  const [loggedInStatus, setLoggedInStatus] = useState("未ログイン");
-  const [user, setUser] = useState({});
+  // const [loggedInStatus, setLoggedInStatus] = useState("未ログイン");
+  // const [user, setUser] = useState({});
 
-  const handleLogin = (data) => {
-    setLoggedInStatus("ログイン中");
-    setUser(data.user);
-  };
+  // const handleLogin = (data) => {
+  //   setLoggedInStatus("ログイン中");
+  //   setUser(data.user);
+  // };
 
   useEffect(() => {
     checkLoginStatus();
@@ -36,37 +32,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/auth">
-          <Auth />
-        </Route>
-
-        <Route exact path="/login">
-          <Login />
-        </Route>
-
-        <Route
-          exact
-          path={"/"}
-          render={(props) => (
-            <Index
-              {...props}
-              handleLogin={handleLogin}
-              loggedInStatus={loggedInStatus}
-            />
-          )}
-        />
-
-        <Route exact path="/">
-          <Index />
-        </Route>
-
-        <Route exact path="/show">
-          <Show />
-        </Route>
-
-        <Route exact path="/create">
-          <Create />
-        </Route>
+        <Routes />
       </Switch>
     </Router>
   );
