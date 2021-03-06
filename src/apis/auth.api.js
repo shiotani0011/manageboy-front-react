714a7c58210c3dc58fc9faf23e921968a18c9e8c
Takehito Tada <taketada.works@gmail.com>
2021-03-07 01:08:40 +0900

    fix: 不要コード削除

--- src/apis/auth.api.js
+++ src/apis/auth.api.js
@@ -3,7 +3,6 @@ import {API_ENDPOINT} from "../config/env";
 
 export const postLogin = async (params) => {
 	try {
-		// const response = await axios.post(`${API_ENDPOINT}/auth/login`, params, {withCredentials: true});
 		const response = await axios.post(`${API_ENDPOINT}/auth/login`, params, {withCredentials: true});
 		return response.data;
 	} catch (error) {
